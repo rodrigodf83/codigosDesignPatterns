e0eeaf50b5c2cd1a6d872fd2401346ade0dbce91
Rodrigo S. Pereira <rodrigo.gringodf@hotmail.com>
2018-02-12 17:46:12 -0200

    padrões chain of responsibility, composite, proxy, decorator, adapter

---
+++ src/chainofresopnsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package chainofresopnsibility;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
